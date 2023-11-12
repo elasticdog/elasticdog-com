19ec15ceaa5550ebbee844dbd947f52542a758df
Aaron Bull Schaefer <aaron@elasticdog.com>
2023-11-12 15:57:51 -0800

    Use elasticdog sticker for footer background and favicon

--- quartz/quartz.config.ts
+++ quartz/quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🐶 ElasticDog",
+    pageTitle: "ElasticDog",
     enableSPA: true,
     enablePopovers: true,
     analytics: null,
