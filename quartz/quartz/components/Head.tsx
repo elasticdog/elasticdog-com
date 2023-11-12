cb5969aa361881cbe2c0cb5555bfc367cac11b3d
Aaron Bull Schaefer <aaron@elasticdog.com>
2023-11-12 14:09:26 -0800

    Add ElasticDog to the end of each page title

--- quartz/quartz/components/Head.tsx
+++ quartz/quartz/components/Head.tsx
@@ -17,7 +17,7 @@ export default (() => {
 
     return (
       <head>
-        <title>{title}</title>
+        <title>{title} | ElasticDog</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta property="og:title" content={title} />
