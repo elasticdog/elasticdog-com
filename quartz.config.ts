864bc452161b9561ac9b355097f2c5d8cc16b2fd
Aaron Bull Schaefer <aaron@elasticdog.com>
2024-11-26 13:42:08 -0800

    Use webp for watch images
    There's a mimetype bug in Quartz where using avif will cause it to think
    you're transcluding a file rather than embedding an image.

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "ElasticDog",
-    pageTitleSuffix: "ElasticDog",
+    pageTitleSuffix: " | ElasticDog",
     enableSPA: true,
     enablePopovers: true,
     analytics: null,
