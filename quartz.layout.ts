9350c25cca9482e81e869af52495aaba4878ce4c
Aaron Bull Schaefer <aaron@elasticdog.com>
2025-06-04 12:18:10 -0700

    style: remove new reader mode feature from rendered site
    This isn't terribly useful, as it really only hides the side bar. I find
    it confusing and not providing much value, so we'll disable it.

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -38,7 +38,6 @@ export const defaultContentPageLayout: PageLayout = {
           grow: true,
         },
         { Component: Component.Darkmode() },
-        { Component: Component.ReaderMode() },
       ],
     }),
     Component.Explorer(),
