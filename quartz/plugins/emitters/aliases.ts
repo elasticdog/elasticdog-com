5d549b4840a93504ece824eb99a0b3496fd3c501
Aaron Bull Schaefer <aaron@elasticdog.com>
2025-07-02 08:58:28 -0700

    Explicitly allow robots to index my site

--- quartz/plugins/emitters/aliases.ts
+++ quartz/plugins/emitters/aliases.ts
@@ -24,7 +24,6 @@ async function* processFile(ctx: BuildCtx, file: VFile) {
         <head>
         <title>${ogSlug}</title>
         <link rel="canonical" href="${redirUrl}">
-        <meta name="robots" content="noindex">
         <meta charset="utf-8">
         <meta http-equiv="refresh" content="0; url=${redirUrl}">
         </head>
