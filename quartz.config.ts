71e98d0597d41908f3c43b260d0e092390bba305
Aaron Bull Schaefer <aaron@elasticdog.com>
2025-09-19 09:08:00 -0700

    Self-host web fonts rather than use Google's CDN
    This wasn't working properly in the past, but it looks like the build
    process is good. They just don't load in the local environment due to
    using the full absolute URL rather than relative paths.

--- quartz.config.ts
+++ quartz.config.ts
@@ -20,7 +20,7 @@ const config: QuartzConfig = {
     generateSocialImages: false,
     theme: {
       fontOrigin: "googleFonts",
-      cdnCaching: true,
+      cdnCaching: false,
       typography: {
         header: "Fira Sans Condensed",
         body: "Literata",
